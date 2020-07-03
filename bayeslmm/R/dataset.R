9ac4f50247acb216258c5bcb69ddb06230bcbe1f
miked-hydroeco <michael.john.dunbar@gmail.com>
2020-07-03 16:47:46 +0100

    add R package

---
+++ bayeslmm/R/dataset.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+data(sleepstudy, package = 'lme4')
+
+sleepstudy_df <- sleepstudy %>%
+  as_tibble() %>%
+  select(rt = Reaction,
+         day = Days,
+         subject = Subject)
+
+usethis::use_data(sleepstudy_df, overwrite = TRUE)
